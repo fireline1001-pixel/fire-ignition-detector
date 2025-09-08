a603a37861a6065cf34fe94789059d29e4d68771
UltralyticsAssistant <web@ultralytics.com>
2025-09-08 04:32:30 +0000

    Auto-format by https://ultralytics.com/actions

--- check_label_classes.py
+++ check_label_classes.py
@@ -6,10 +6,10 @@ for root, _, files in os.walk(label_dir):
     for file in files:
         if file.endswith(".txt"):
             file_path = os.path.join(root, file)
-            with open(file_path, "r", encoding="utf-8") as f:
+            with open(file_path, encoding="utf-8") as f:
                 lines = f.readlines()
                 for line in lines:
-                    if line.strip() == '':
+                    if line.strip() == "":
                         continue
                     class_id = int(line.split()[0])
                     if class_id > 0:
